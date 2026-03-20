588978e353cf879f6c346bb92dbd15b9ebfd33ce
Ram Charan G Gaddamidhi <ramgoud696@gmail.com>
2026-03-20 22:25:18 +0530

    s
    s

--- src/app/components/ProgramsSection.js
+++ src/app/components/ProgramsSection.js
@@ -2,7 +2,10 @@ import ActivitiesSection from "./ActivitiesSection";
 
 export default function ProgramsSection() {
   return (
-    <section className="px-4 bg-[#e3f0fa]">
+    <section
+      className="px-4 bg-[#e3f0fa]"
+      style={{ fontFamily: "'Datatype', 'Iosevka Charon Mono', Arial, Helvetica, sans-serif" }}
+    >
       <ActivitiesSection/>
     </section>
   );
